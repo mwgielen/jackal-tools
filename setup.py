ad9fbae0ee2633471b2abb77a9cbb2a23d713e85
Matthijs Gielen <github@mwgielen.com>
2018-01-13 17:03:28 +0100

    Added sniffer tool.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='jackal-tools',
+      version='0.0.1',
+      description='Tools for jackal.',
+      author='Matthijs Gielen',
+      author_email='github@mwgielen.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      requires_python='>=3',
+      url='https://github.com/mwgielen/jackal-tools/',
+      packages=['jackal_tools'],
+      install_requires=['jackal', 'psutil', 'scapy-python3'],
+      entry_points={
+          'console_scripts': [
+              'jk-sniffer = jackal_tools.sniffer:main',
+          ]
+      })
